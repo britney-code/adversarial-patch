a088947ae1960fdcd49709321e4ef59d154ac23c
britney-code <1370192111@qq.com>
2025-03-03 16:14:26 +0800

    Add files via upload

--- train/train.py
+++ train/train.py
@@ -14,29 +14,36 @@ import numpy as np
 import time 
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
-# resnet [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+# resnet resize 299 [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
 # inceptionv3  resize 299, [0.5, 0.5, 0.5], [0.5, 0.5, 0.5]
 def process():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
 
     data_transform = {
-        "train": transforms.Compose(
-            [transforms.RandomResizedCrop(299),
-             transforms.RandomHorizontalFlip(),
-             transforms.ToTensor(),
-             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        "train": transforms.Compose([
+            transforms.RandomResizedCrop(299, scale = (0.8, 1.0)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomRotation(degrees = 45),
+            transforms.RandomGrayscale(p=0.2),
+            # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            # transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
+            transforms.RandomPerspective(distortion_scale=0.5, p=0.5),
+            transforms.ToTensor(),
+            # transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         "val": transforms.Compose(
             [transforms.Resize((299, 299)),
              transforms.ToTensor(),
-             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
     }
 
     # data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
     # image_path = os.path.join(data_root, "data_set1", "flower_data1")  # flower data set path
-    image_path = './dataset/'
+    image_path = '../dataset/'
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
     train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
                                          transform=data_transform["train"])
@@ -50,7 +57,7 @@ def process():
     with open('class_indices.json', 'w') as json_file:
         json_file.write(json_str)
 
-    batch_size = 25
+    batch_size = 50
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     print('Using {} dataloader workers every process'.format(nw))
 
@@ -64,11 +71,10 @@ def process():
                                                   num_workers=nw)
     print("using {} images for training, {} images for validation.".format(train_num, val_num))
 
-    net = inception_v3(pretrained=False, num_classes = 5, aux_logits=False).to(device)
+    net = resnet50(num_classes = 5).to(device)
     loss = nn.CrossEntropyLoss()
     epoch = 100 
     lr = 1e-4
-
     return  net, loss, train_loader, val_loader, device, epoch, lr  
 
 def train(net, loss, train_dataloader, valid_dataloader, device, num_epoch, lr, optim='adam',scheduler_type='Cosine', init = True, checkpoint = None):
@@ -91,9 +97,9 @@ def train(net, loss, train_dataloader, valid_dataloader, device, num_epoch, lr, 
     # elif optim == 'ranger':
     #     optimizer = Ranger((param for param in net.parameters() if param.requires_grad), lr=lr,
     #                        weight_decay=0)
-    # if scheduler_type == 'Cosine':
-    #     lr_min = 0
-    #     scheduler = CosineAnnealingLR(optimizer, T_max=num_epoch, eta_min=lr_min)
+    if scheduler_type == 'Cosine':
+         lr_min = 0
+         scheduler = CosineAnnealingLR(optimizer, T_max=num_epoch, eta_min=lr_min)
 
     # 用来保存每个epoch的Loss和acc以便最后画图
     train_losses = []
@@ -101,9 +107,10 @@ def train(net, loss, train_dataloader, valid_dataloader, device, num_epoch, lr, 
     eval_losses = []
     eval_acces = []
     best_acc = 0.0
-    path = './checkpoint/' 
-    if checkpoint is not None: 
+    path = '../checkpoint/'
+    if checkpoint: 
         best_acc = eval(path, net, valid_dataloader, loss)
+        print(f"======>包含模型权重，当前的最好的验证级准确率:[{best_acc / len(valid_dataloader)}]<======")
     
     # path = ".\\checkpoint\\" + net.__class__.__name__ + '-latest'+ '.pth'
     early_stopping = EarlyStopping(verbose=True)
@@ -129,7 +136,7 @@ def train(net, loss, train_dataloader, valid_dataloader, device, num_epoch, lr, 
             train_acc += num_correct / imgs.shape[0]
             train_loss += Loss.item()
 
-        # scheduler.step()
+        scheduler.step()
         train_acces.append(train_acc / len(train_dataloader))
         train_losses.append(train_loss / len(train_dataloader))
 
@@ -193,10 +200,11 @@ def eval(path, net, val_loader, loss):
 
 
 if __name__ == '__main__':
-    # path = "./checkpoint"
+    path = "../checkpoint"
     net, loss, train_loader, val_loader, device, epoch, lr  = process()
-    # net.load_state_dict(torch.load(os.path.join(path, net.__class__.__name__ + '-best' + '.pth')))
-    # eval(net, val_loader, loss)
-    train_losses, train_acces, eval_losses, eval_acces = train(net, loss, train_loader, val_loader, device, epoch, lr)
-    show_acces(train_losses, train_acces,eval_losses ,eval_acces, num_epoch=1)
+    net.load_state_dict(torch.load(os.path.join(path, net.__class__.__name__ + '-best' + '.pth')))
+    train_losses, train_acces, eval_losses, eval_acces = train(
+        net, loss, train_loader, val_loader, device, epoch, lr, checkpoint=True
+    )
+    # show_acces(train_losses, train_acces,eval_losses ,eval_acces, num_epoch=1)
 
